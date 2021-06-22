f89533dea00b2b9fbe78fe398856c41972d4fa2e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-06-22 17:53:22 -0300

    aula 02 finished: authenticate firebase and create context auth

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from './components/Button';
+import { Home } from './pages/Home';
+
+import { BrowserRouter, Route } from 'react-router-dom';
+import { NewRoom } from './pages/NewRoom';
+
+
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
   return (
-    <div>
-      <Button>Clique Aqui</Button>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
