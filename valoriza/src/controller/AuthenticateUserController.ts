0cea4163c6873d23c7f0afaa1d14829012eb5c20
Raphael Feitosa <raphaelcs2@gmail.com>
2021-06-30 23:17:09 -0300

    update token to accessToken AuthenticadeUserController

--- valoriza/src/controller/AuthenticateUserController.ts
+++ valoriza/src/controller/AuthenticateUserController.ts
@@ -4,17 +4,17 @@ import { AuthenticateUserService } from "../services/AuthenticadeUserService";
 
 class AuthenticateUserController {
 
-    async handle(request: Request, response: Response) {
-        const { email, password } = request.body;
+  async handle(request: Request, response: Response) {
+    const { email, password } = request.body;
 
-        const authenticateUserService = new AuthenticateUserService();
+    const authenticateUserService = new AuthenticateUserService();
 
-        const token = await authenticateUserService.execute({
-            email,
-            password
-        });
+    const accessToken = await authenticateUserService.execute({
+      email,
+      password
+    });
 
-        return response.json(token);
-    }
+    return response.json({ accessToken: accessToken });
+  }
 }
 export { AuthenticateUserController };
