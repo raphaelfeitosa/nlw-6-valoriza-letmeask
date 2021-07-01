bea29065de592c28d364ecc232f6764abeee8baf
Raphael Feitosa <raphaelcs2@gmail.com>
2021-06-30 23:05:36 -0300

    throw hapi error and process.env AuthenticateUserService

--- valoriza/src/services/AuthenticadeUserService.ts
+++ valoriza/src/services/AuthenticadeUserService.ts
@@ -1,34 +1,36 @@
+import { badRequest } from "@hapi/boom";
 import { getCustomRepository, Repository } from "typeorm";
 import { compare } from "bcryptjs";
 import { UsersRepositories } from "../repositories/UsersRepositories";
 import { sign } from 'jsonwebtoken';
 
 interface IAuthenticateRequest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 class AuthenticateUserService {
 
-    async execute({ email, password }: IAuthenticateRequest) {
+  async execute({ email, password }: IAuthenticateRequest) {
 
-        const usersRepositories = getCustomRepository(UsersRepositories);
+    const usersRepositories = getCustomRepository(UsersRepositories);
 
-        const user = await usersRepositories.findOne({ email });
+    const user = await usersRepositories.findOne({ email });
 
-        if (!user) throw new Error("Email/password incorrect");
+    if (!user) throw badRequest("Email/password incorrect");
 
-        const passwordMatch = await compare(password, user.password);
+    const passwordMatch = await compare(password, user.password);
 
-        if (!passwordMatch) throw new Error("Email/password incorrect");
+    if (!passwordMatch) throw badRequest("Email/password incorrect");
 
-        const token = sign({
-            email: user.email
-        }, "5b40e996d5e2fd0b8a77da09e0585edd", {
-            subject: user.id, expiresIn: "1D"
-        });
+    const token = sign({
+      email: user.email
+    }, process.env.JWT_SECRET, {
+      subject: user.id,
+      expiresIn: process.env.JWT_EXPIRATION_TIME,
+    });
 
-        return token;
-    }
+    return token;
+  }
 }
 
 export { AuthenticateUserService };
