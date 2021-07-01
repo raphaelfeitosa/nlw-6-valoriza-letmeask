c5e2290218c9d3a24c7282f571ba7f1cb2ac5fa5
Raphael Feitosa <raphaelcs2@gmail.com>
2021-06-30 23:24:40 -0300

    throw hapi error CreateComplimentService

--- valoriza/src/services/CreateComplimentService.ts
+++ valoriza/src/services/CreateComplimentService.ts
@@ -1,35 +1,39 @@
+import { badRequest, notFound } from "@hapi/boom";
 import { getCustomRepository } from "typeorm";
 import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories";
 import { UsersRepositories } from "../repositories/UsersRepositories";
 
 
 interface IComplementRequest {
-    tag_id: string;
-    user_sender: string;
-    user_receiver: string;
-    message: string;
+  tag_id: string;
+  user_sender: string;
+  user_receiver: string;
+  message: string;
 }
 class CreateComplementService {
-    async execute({ tag_id, user_sender, user_receiver, message }: IComplementRequest) {
-        const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
-        const usersRepositories = getCustomRepository(UsersRepositories);
+  async execute({ tag_id, user_sender, user_receiver, message }: IComplementRequest) {
+    const complimentsRepositories = getCustomRepository(ComplimentsRepositories);
+    const usersRepositories = getCustomRepository(UsersRepositories);
 
-        if (user_sender === user_receiver) throw new Error("Incorrect User Receiver!");
+    if (user_sender === user_receiver) throw badRequest(
+      'Is not allowed create a compliment from and to the same user',
+      { code: 340 }
+    );
 
-        const userReceiverExists = await usersRepositories.findOne(user_receiver);
+    const userReceiverExists = await usersRepositories.findOne(user_receiver);
 
-        if (!userReceiverExists) throw new Error("User Receiver dos not exists!");
+    if (!userReceiverExists) throw notFound('Receiver user not found', { code: 344 });
 
-        const compliment = complimentsRepositories.create({
-            tag_id,
-            user_receiver,
-            user_sender,
-            message
-        });
+    const compliment = complimentsRepositories.create({
+      tag_id,
+      user_receiver,
+      user_sender,
+      message
+    });
 
-        await complimentsRepositories.save(compliment);
+    await complimentsRepositories.save(compliment);
 
-        return compliment;
-    }
+    return compliment;
+  }
 }
 export { CreateComplementService };
