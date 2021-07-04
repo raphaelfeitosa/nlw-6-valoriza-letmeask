effa8be612893332f32e452b338ad5cc6b78918b
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-04 16:41:42 -0300

    add container/index ITagRepository and TagRepository

--- valoriza/src/shared/container/index.ts
+++ valoriza/src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { ITagsRepository, TagsRepository } from "@modules/tags/infra/repositories/tagRepository";
+// import { TagsRepository } from "@modules/tags/infra/repositories/tagRepository/TagsRepository";
 import { IUsersRepository } from "@modules/users/infra/repositories/userRepository/IUsersRepository";
 import { UsersRepository } from "@modules/users/infra/repositories/userRepository/UserRepository";
 import { container } from "tsyringe";
@@ -5,4 +7,9 @@ import { container } from "tsyringe";
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
-)
+);
+
+container.registerSingleton<ITagsRepository>(
+  "TagsRepository",
+  TagsRepository
+);
