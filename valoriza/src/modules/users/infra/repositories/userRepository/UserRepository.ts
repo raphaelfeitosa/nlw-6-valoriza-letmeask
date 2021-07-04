7226dc2f27711cc017be9614c04293946be7b10e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-04 15:17:42 -0300

    add contract interface userRepository

---
+++ valoriza/src/modules/users/infra/repositories/userRepository/UserRepository.ts
@@ -0,0 +1,38 @@
+import { getRepository, Repository } from "typeorm";
+import { User } from "@modules/users/infra/typeorm/entities/User";
+import { IUsersRepository } from "./IUsersRepository";
+import { ICreateUserDTO } from "@modules/users/dtos/ICreateUserDTO";
+
+class UsersRepository implements IUsersRepository {
+  private userRepository: Repository<User>;
+
+  constructor() {
+    this.userRepository = getRepository(User);
+  }
+
+  async create({ name, email, password, admin }: ICreateUserDTO): Promise<void> {
+    const user = this.userRepository.create({
+      name,
+      email,
+      password,
+      admin
+    });
+    await this.userRepository.save(user);
+  }
+
+  findAll(): Promise<User[]> {
+    const users = this.userRepository.find();
+    return users;
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.userRepository.findOne({ email });
+    return user;
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = await this.userRepository.findOne(id);
+    return user;
+  }
+}
+export { UsersRepository };
