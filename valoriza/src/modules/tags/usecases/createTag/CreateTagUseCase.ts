4bd19562a72ea656efd78d504092ae1352e546f0
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-04 17:12:12 -0300

    update name and add inject tsryng

---
+++ valoriza/src/modules/tags/usecases/createTag/CreateTagUseCase.ts
@@ -0,0 +1,28 @@
+import { badRequest } from "@hapi/boom";
+import { inject, injectable } from "tsyringe"
+
+import { ICreateTagDTO } from "@modules/tags/dtos/ICreateTagDTO";
+import { ITagsRepository } from "@modules/tags/infra/repositories/tagRepository";
+import { Tag } from "@modules/tags/infra/typeorm/entities/Tag";
+
+@injectable()
+export class CreateTagUseCase {
+  constructor(
+    @inject("TagsRepository")
+    private tagsRepository: ITagsRepository
+  ) { }
+
+  async execute({ name }: ICreateTagDTO): Promise<Tag> {
+    const tagAlreadyExists = await this.tagsRepository.findByName(name);
+
+    if (tagAlreadyExists) {
+      throw badRequest("Tag Already Exists", { code: 240 });
+    }
+
+    const tag = await this.tagsRepository.create({
+      name
+    });
+
+    return tag;
+  }
+}
