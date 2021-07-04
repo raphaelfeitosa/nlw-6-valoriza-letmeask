1f484af02d873a50412a0ae20b87a1ad5ff6780e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-04 17:11:41 -0300

    delete files to improve organization

--- valoriza/src/modules/tags/infra/repositories/TagsRepositories.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { Tag } from "../typeorm/entities/Tag";
-
-@EntityRepository(Tag)
-class TagsRepositories extends Repository<Tag>{
-}
-export { TagsRepositories };
