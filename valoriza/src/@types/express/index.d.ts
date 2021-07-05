fb20a9e4bcd7af5d61b91fce02880df512def417
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-05 18:21:20 -0300

    configure types to import

--- valoriza/src/@types/express/index.d.ts
+++ valoriza/src/@types/express/index.d.ts
@@ -1,5 +1,7 @@
 declare namespace Express {
-    export interface Request {
-        user_id: string;
-    }
+  export interface Request {
+    user_id: string;
+    hostUrl: string;
+    currentUrl: string;
+  }
 }
