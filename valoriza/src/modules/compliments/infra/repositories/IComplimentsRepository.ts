32751c97a4072281862d37a7119f2e679ab3d5be
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-05 18:15:35 -0300

    add count

--- valoriza/src/modules/compliments/infra/repositories/IComplimentsRepository.ts
+++ valoriza/src/modules/compliments/infra/repositories/IComplimentsRepository.ts
@@ -4,7 +4,9 @@ import { Compliment } from "../typeorm/entities";
 export interface IComplimentRepository {
   create(data: ICreateComplimentDTO): Promise<Compliment>;
   findAll(): Promise<Compliment[]>;
-  findAllUserReceiver(user_receiver: string): Promise<Compliment[]>;
-  findAllUserSender(user_sender: string): Promise<Compliment[]>;
+  findAllUserReceiver(user_receiver: string, limit: number, page: number): Promise<Compliment[]>;
+  findAllUserSender(user_sender: string, limit: number, page: number): Promise<Compliment[]>;
   findById(id: string): Promise<Compliment>;
+  countUserReceiver(user_receiver: string): Promise<number>;
+  countUserSender(user_sender: string): Promise<number>;
 }
