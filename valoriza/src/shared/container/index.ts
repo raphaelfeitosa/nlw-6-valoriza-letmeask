8546fa45a1d3778689aa93c3c4228b88f5d03ea9
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-04 23:29:37 -0300

    add inject Compliment tsryng

--- valoriza/src/shared/container/index.ts
+++ valoriza/src/shared/container/index.ts
@@ -2,6 +2,7 @@ import { container } from "tsyringe";
 
 import { ITagsRepository, TagsRepository } from "@modules/tags/infra/repositories/tagRepository";
 import { IUsersRepository, UsersRepository } from "@modules/users/infra/repositories/userRepository";
+import { ComplimentsRepository, IComplimentRepository } from "@modules/compliments/infra/repositories";
 
 container.registerSingleton<IUsersRepository>(
   "UsersRepository",
@@ -11,4 +12,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ITagsRepository>(
   "TagsRepository",
   TagsRepository
+);
+
+container.registerSingleton<IComplimentRepository>(
+  "ComplimentsRepository",
+  ComplimentsRepository
 );
