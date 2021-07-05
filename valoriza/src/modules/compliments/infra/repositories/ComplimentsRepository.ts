dfe5f71583f2b43dba9db5167263c5a1529b1d6a
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-05 18:17:03 -0300

    create metodo count and paginate results

--- valoriza/src/modules/compliments/infra/repositories/ComplimentsRepository.ts
+++ valoriza/src/modules/compliments/infra/repositories/ComplimentsRepository.ts
@@ -31,22 +31,34 @@ export class ComplimentsRepository implements IComplimentRepository {
     return compliments;
   }
 
-  async findAllUserReceiver(user_receiver: string): Promise<Compliment[]> {
+  async findAllUserReceiver(
+    user_receiver: string,
+    page: number,
+    limit: number
+  ): Promise<Compliment[]> {
     const complimentsAllUserReceiver = await this.complimentRepository.find({
       where: {
         user_receiver
       },
       relations: ["userSender", "tag"],
+      take: limit,
+      skip: (page - 1) * limit
     });
     return complimentsAllUserReceiver;
   }
 
-  async findAllUserSender(user_sender: string): Promise<Compliment[]> {
+  async findAllUserSender(
+    user_sender: string,
+    page: number,
+    limit: number
+  ): Promise<Compliment[]> {
     const complimentsAllUserSender = await this.complimentRepository.find({
       where: {
         user_sender
       },
-      relations: ["userReceiver", "tag"]
+      relations: ["userReceiver", "tag"],
+      skip: (page - 1) * limit,
+      take: limit
     });
     return complimentsAllUserSender;
   }
@@ -55,4 +67,19 @@ export class ComplimentsRepository implements IComplimentRepository {
     const compliment = await this.complimentRepository.findOne(id);
     return compliment;
   }
+
+  async countUserReceiver(user_receiver: string): Promise<number> {
+    const countUserReceiver = await this.complimentRepository.count({
+      user_receiver
+    });
+    return countUserReceiver;
+  }
+
+  async countUserSender(user_sender: string): Promise<number> {
+    const countUserSender = await this.complimentRepository.count({
+      user_sender
+    });
+    return countUserSender;
+  }
+
 }
