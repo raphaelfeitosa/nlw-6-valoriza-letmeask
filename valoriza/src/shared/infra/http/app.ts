ba14d2bbb8decfb8874c9742f979e36a99589e7e
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-06 10:37:13 -0300

    add configuration swagger app

--- valoriza/src/shared/infra/http/app.ts
+++ valoriza/src/shared/infra/http/app.ts
@@ -7,9 +7,12 @@ import helmet from "helmet";
 import { errors } from "celebrate";
 import { isBoom } from '@hapi/boom';
 
+import swaggerUI from "swagger-ui-express";
+
 import "@shared/infra/typeorm";
 import "@shared/container";
 
+import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 import { routeAliases } from "@shared/infra/http/middlewares";
 
@@ -20,6 +23,8 @@ app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(routeAliases);
 
