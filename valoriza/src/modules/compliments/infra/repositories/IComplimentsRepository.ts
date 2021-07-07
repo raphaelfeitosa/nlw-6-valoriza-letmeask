8884844cda11cf5cccd498c9e4495e4b6d37c9c7
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-06 23:26:11 -0300

    add interface countCompliments and parameters findAll

--- valoriza/src/modules/compliments/infra/repositories/IComplimentsRepository.ts
+++ valoriza/src/modules/compliments/infra/repositories/IComplimentsRepository.ts
@@ -3,10 +3,11 @@ import { Compliment } from "../typeorm/entities";
 
 export interface IComplimentRepository {
   create(data: ICreateComplimentDTO): Promise<Compliment>;
-  findAll(): Promise<Compliment[]>;
+  findAll(page: number, limit: number): Promise<Compliment[]>;
   findAllUserReceiver(user_receiver: string, limit: number, page: number): Promise<Compliment[]>;
   findAllUserSender(user_sender: string, limit: number, page: number): Promise<Compliment[]>;
   findById(id: string): Promise<Compliment>;
   countUserReceiver(user_receiver: string): Promise<number>;
   countUserSender(user_sender: string): Promise<number>;
+  countCompliments(): Promise<number>;
 }
