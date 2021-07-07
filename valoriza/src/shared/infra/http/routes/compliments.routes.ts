2820f75666788edf4a29db2f5d951ecddd6b7fd1
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-06 23:29:34 -0300

    add router get list all compliments

--- valoriza/src/shared/infra/http/routes/compliments.routes.ts
+++ valoriza/src/shared/infra/http/routes/compliments.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import { ListUserReceiverComplimentsController } from "@modules/compliments/usecases/listUserReceiverCompliments";
 import { ListUserSenderComplimentsController } from "@modules/compliments/usecases/listUserSenderCompliments";
+import { ListComplimentsController } from "@modules/compliments/usecases/listCompliments";
 import { CreateComplimentController } from "@modules/compliments/usecases/createCompliment";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares";
 import complimentValidator from "@shared/validators/complimentValidator";
@@ -9,10 +10,16 @@ import pageValidator from "@shared/validators/pageValidator";
 
 const listUserReceiverComplimentsController = new ListUserReceiverComplimentsController();
 const listUserSenderComplimentsController = new ListUserSenderComplimentsController();
+const listComplimentsController = new ListComplimentsController();
 const createComplimentController = new CreateComplimentController();
 
 const complimentsRoutes = Router();
 
+complimentsRoutes.get("/",
+  ensureAuthenticated,
+  pageValidator,
+  listComplimentsController.handle
+);
 complimentsRoutes.get("/send",
   ensureAuthenticated,
   pageValidator,
