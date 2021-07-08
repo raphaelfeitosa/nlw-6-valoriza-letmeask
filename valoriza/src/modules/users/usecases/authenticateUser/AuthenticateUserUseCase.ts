28a7685e051c71b634a924b5ad3c79a6415249de
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-08 01:03:11 -0300

    update and inject refreshtoken, date provider and dtos

--- valoriza/src/modules/users/usecases/authenticateUser/AuthenticateUserUseCase.ts
+++ valoriza/src/modules/users/usecases/authenticateUser/AuthenticateUserUseCase.ts
@@ -4,27 +4,54 @@ import { compare } from "bcryptjs";
 import { sign } from 'jsonwebtoken';
 
 import auth from "@config/auth";
-import { ICreateUserTokenDTO } from "@modules/users/dtos";
 import { IUsersRepository } from "@modules/users/infra/repositories/userRepository";
+import { IDateProvider } from "@shared/container/providers/dateProvider";
+import { IUsersTokensRepository } from "@modules/users/infra/repositories/userToken";
+import { IRequestAuthenticateDTO, IResponseAuthenticateDTO } from "@modules/users/dtos";
 
 @injectable()
 export class AuthenticateUserUseCase {
   constructor(
     @inject("UsersRepository")
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject("DaysJSDateProvider")
+    private dateProvider: IDateProvider
   ) { }
 
-  async execute({ email, password }: ICreateUserTokenDTO): Promise<any> {
+  async execute({ email, password }: IRequestAuthenticateDTO): Promise<IResponseAuthenticateDTO> {
     const user = await this.usersRepository.findByEmail(email);
     if (!user) throw badRequest("Email/password incorrect");
     const passwordMatch = await compare(password, user.password);
     if (!passwordMatch) throw badRequest("Email/password incorrect");
-    const token = sign({
-      email: user.email
-    }, auth.secret_token, {
+    const token = sign({},
+      auth.secret_token, {
       subject: user.id,
-      expiresIn: process.env.JWT_EXPIRATION_TIME,
+      expiresIn: auth.expires_in_token,
     });
-    return token;
+    const refresh_token = sign({
+      email
+    },
+      auth.secret_refresh_token, {
+      subject: user.id,
+      expiresIn: auth.expires_in_refresh_token
+    });
+    const refresh_token_expires_date =
+      this.dateProvider.addDays(auth.expires_refresh_token_days);
+    await this.usersTokensRepository.create({
+      expires_date: refresh_token_expires_date,
+      refresh_token,
+      user_id: user.id
+    });
+    const tokenReturn: IResponseAuthenticateDTO = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      },
+      refresh_token
+    }
+    return tokenReturn;
   }
 }
