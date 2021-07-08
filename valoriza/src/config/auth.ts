aba31b7557783240a8c13c85b428a32b500fd68f
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-07 22:43:37 -0300

    add atributes refresh token

--- valoriza/src/config/auth.ts
+++ valoriza/src/config/auth.ts
@@ -1,4 +1,7 @@
 export default {
   secret_token: "69acf12ae6c9b2ca3e3e71dc6793bb91",
-  expires_in_token: "15m"
+  expires_in_token: "15m",
+  secret_refresh_token: "790b5fc5ef5d3807570ba28e312c7de4",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30
 }
