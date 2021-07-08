c25a97718fb971697035bfdcd2de149576bfd0e7
Raphael Feitosa <raphaelcs2@gmail.com>
2021-07-08 01:05:59 -0300

    update index

--- valoriza/src/shared/infra/http/routes/index.ts
+++ valoriza/src/shared/infra/http/routes/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
-import { sessionsRoutes } from "./sessions.routes";
+import { authenticateRoutes } from "./authenticate.routes";
 import { tagsRoutes } from "./tags.routes";
 import { usersRoutes } from "./users.routes";
 import { complimentsRoutes } from "./compliments.routes";
 
 const router = Router();
 
-router.use('/sessions', sessionsRoutes);
+router.use(authenticateRoutes);
 router.use('/users', usersRoutes);
 router.use('/tags', tagsRoutes);
 router.use('/compliments', complimentsRoutes);
